183e05be874dffc05875b6d59d9780ad16332e87
Pavlina <pandonova95@gmail.com>
2021-12-08 12:40:23 +0200

    Init ScandyPWA app.

---
+++ magento/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/scandipwa/scandy-pwa-app',
+    __DIR__
+);
